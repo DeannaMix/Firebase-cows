c483b263d62d08e1da0247ac52dfdc9d2fed456d
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-09-22 14:26:03 -0500

    added data for cows, farmer, and pasture

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import pasture from '../../components/pasture/pasture';
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       $('#auth').addClass('hide');
       $('#pasture').removeClass('hide');
-      $('#pasture').html(
-        `<img src="${user.photoURL}" alt="${user.displayName}">}`
-      );
       $('#navbar-logout-button').removeClass('hide');
+      pasture.buildCows();
     } else {
       $('#navbar-logout-button').addClass('hide');
       $('#auth').removeClass('hide');
